2ed606600393bbc8b140dff6580ecfbd630b50e0
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-02 22:39:11 +0700

    Update

--- index.php
+++ index.php
@@ -21,32 +21,33 @@ namespace x\comment__guard__link {
         return $y;
     }
     function route($content, $path, $query) {
-        if ('POST' === $_SERVER['REQUEST_METHOD']) {
-            \extract($GLOBALS, \EXTR_SKIP);
-            // Remove the `link` data before it gets to the comment saving process route
-            unset($_POST['comment']['link']);
-            // Limit number of link(s) in comment content
-            $max = (int) ($state->x->{'comment.guard.link'}->content ?? 5);
-            if ($max >= 0) {
-                $test = $_POST['comment']['content'] ?? "";
-                if (\substr_count(\strtolower($test), '</a>') > $max) {
+        if ('POST' !== $_SERVER['REQUEST_METHOD']) {
+            return $content;
+        }
+        \extract($GLOBALS, \EXTR_SKIP);
+        // Limit number of link(s) in comment content
+        $max = (int) ($state->x->{'comment.guard.link'}->content ?? 5);
+        if ($max >= 0) {
+            $test = $_POST['comment']['content'] ?? "";
+            if (\substr_count(\strtolower($test), '</a>') > $max) {
+                \class_exists("\\Alert") && \Alert::error('Too many links in the comment.');
+                foreach (['author', 'content', 'email'] as $v) {
+                    $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
+                }
+                \kick($path . $query . '#comment');
+            }
+            if (false !== \strpos($test, '://') && \preg_match_all('/\bhttps?:\/\/\S+/', \strip_tags($test), $m)) {
+                if (\count($m[0]) > $max) {
                     \class_exists("\\Alert") && \Alert::error('Too many links in the comment.');
                     foreach (['author', 'content', 'email'] as $v) {
                         $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
                     }
                     \kick($path . $query . '#comment');
                 }
-                if (false !== \strpos($test, '://') && \preg_match_all('/\bhttps?:\/\/\S+/', \strip_tags($test), $m)) {
-                    if (\count($m[0]) > $max) {
-                        \class_exists("\\Alert") && \Alert::error('Too many links in the comment.');
-                        foreach (['author', 'content', 'email'] as $v) {
-                            $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
-                        }
-                        \kick($path . $query . '#comment');
-                    }
-                }
             }
         }
+        // Remove the submitted `link` data before it gets to the comment saving process route
+        unset($_POST['comment']['link']);
         return $content;
     }
     $link = $state->x->{'comment.guard.link'}->link ?? 0;
