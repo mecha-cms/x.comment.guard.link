d77c2a8befa2dd383b8dafbea6cc0bf1349af96d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-11 20:15:26 +0700

    Stable

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Comment Link Guard Extension for [Mecha](https://github.com/mecha-cms/mecha)
 ============================================================================
 
 ![Code Size](https://img.shields.io/github/languages/code-size/mecha-cms/x.comment.guard.link?color=%23444&style=for-the-badge)
+
+Limit the number of links in comments with this extension.
--- index.php
+++ index.php
@@ -5,8 +5,47 @@ if (!isset($state->x->comment)) {
     return;
 }
 
-function y__comment($y) {
+function route__comment($content, $path, $query, $hash) {
+    if ('POST' !== $_SERVER['REQUEST_METHOD']) {
+        return $content;
+    }
     \extract($GLOBALS, \EXTR_SKIP);
+    if (isset($state->x->user) && \Is::user()) {
+        return $content; // Disable the security if current user is logged-in
+    }
+    $can_alert = \class_exists("\\Alert");
+    // Limit number of link(s) in the comment
+    $max = $state->x->{'comment.guard.link'}->content ?? 5;
+    $max = $max < 0 || false === $max ? 0 : $max;
+    if (true !== $max) {
+        $test = $_POST['comment']['content'] ?? "";
+        if (\substr_count(\strtolower($test), '</a>') > $max) {
+            foreach (['author', 'content', 'email'] as $v) {
+                $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
+            }
+            $can_alert && \Alert::error(0 === $max ? 'Links are not allowed in the comment.' : 'Too many links in the comment.');
+            \kick($path . $query . ($hash ?? '#comment'));
+        }
+        if (false !== \strpos($test, '://') && \preg_match_all('/\bhttps?:\/\/\S+/', \strip_tags($test), $m)) {
+            if (\count($m[0]) > $max) {
+                foreach (['author', 'content', 'email'] as $v) {
+                    $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
+                }
+                $can_alert && \Alert::error(0 === $max ? 'Links are not allowed in the comment.' : 'Too many links in the comment.');
+                \kick($path . $query . ($hash ?? '#comment'));
+            }
+        }
+    }
+    $link = $state->x->{'comment.guard.link'}->link ?? 0;
+    if (false === $link || $link < 1) {
+        // Remove the submitted `link` data before it gets to the comment saving process route
+        unset($_POST['comment']['link']);
+    }
+    return $content;
+}
+
+function y__comment($y, $lot) {
+    \extract($lot, \EXTR_SKIP);
     $content = (int) ($state->x->{'comment.guard.link'}->content ?? 5);
     $link = (int) ($state->x->{'comment.guard.link'}->link ?? 0);
     // Strip anchor tag(s) in comment content
@@ -23,53 +62,19 @@ function y__comment($y) {
     return $y;
 }
 
-\Hook::set('y.comment', __NAMESPACE__ . "\\y__comment", 100);
-
-$link = $state->x->{'comment.guard.link'}->link ?? 0;
-if (false === $link || $link < 1) {
-    function route__comment($content, $path, $query, $hash) {
-        if ('POST' !== $_SERVER['REQUEST_METHOD']) {
-            return $content;
-        }
-        \extract($GLOBALS, \EXTR_SKIP);
-        if (isset($state->x->user) && \Is::user()) {
-            return $content; // Disable the security if current user is logged-in
-        }
-        // Limit number of link(s) in the comment
-        $max = $state->x->{'comment.guard.link'}->content ?? 5;
-        $max = $max < 0 || false === $max ? 0 : $max;
-        if (true !== $max) {
-            $test = $_POST['comment']['content'] ?? "";
-            if (\substr_count(\strtolower($test), '</a>') > $max) {
-                foreach (['author', 'content', 'email'] as $v) {
-                    $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
-                }
-                \class_exists("\\Alert") && \Alert::error(0 === $max ? 'Links are not allowed in the comment.' : 'Too many links in the comment.');
-                \kick($path . $query . ($hash ?? '#comment'));
-            }
-            if (false !== \strpos($test, '://') && \preg_match_all('/\bhttps?:\/\/\S+/', \strip_tags($test), $m)) {
-                if (\count($m[0]) > $max) {
-                    foreach (['author', 'content', 'email'] as $v) {
-                        $_SESSION['form']['comment'][$v] = $_POST['comment'][$v] ?? null;
-                    }
-                    \class_exists("\\Alert") && \Alert::error(0 === $max ? 'Links are not allowed in the comment.' : 'Too many links in the comment.');
-                    \kick($path . $query . ($hash ?? '#comment'));
-                }
-            }
-        }
-        // Remove the submitted `link` data before it gets to the comment saving process route
-        unset($_POST['comment']['link']);
-        return $content;
+function y__form__comment($y, $lot) {
+    \extract($lot, \EXTR_SKIP);
+    if (isset($state->x->user) && \Is::user()) {
+        return $y; // Disable the security if current user is logged-in
     }
-    function y__form__comment($y) {
-        \extract($GLOBALS, \EXTR_SKIP);
-        if (isset($state->x->user) && \Is::user()) {
-            return $y; // Disable the security if current user is logged-in
-        }
-        // Remove `link` field in the default comment form
+    $link = $state->x->{'comment.guard.link'}->link ?? 0;
+    if (false === $link || $link < 1) {
+        // Remove the link field in the default comment form
         unset($y[1]['link']);
-        return $y;
     }
-    \Hook::set('route.comment', __NAMESPACE__ . "\\route__comment", 0);
-    \Hook::set('y.form.comment', __NAMESPACE__ . "\\y__form__comment", 100);
+    return $y;
 }
+
+\Hook::set('route.comment', __NAMESPACE__ . "\\route__comment", 0);
+\Hook::set('y.comment', __NAMESPACE__ . "\\y__comment", 100);
+\Hook::set('y.form.comment', __NAMESPACE__ . "\\y__form__comment", 100);
